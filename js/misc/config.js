de0ed9690fb8b5fc696f66ead30ef4e597f86fdf
Rafael Santos <topera@gmail.com>
2011-08-02 07:34:45 -0300

    fixing GA

--- js/misc/config.js
+++ js/misc/config.js
@@ -5,9 +5,9 @@
  * 4 - trace
  * 5 - debug
  */
-var ENV = "dev"; // dev or prd
+var ENV = "prd"; // dev or prd
 var LOG_DEEP = 2;
 var USERNAME = 'topera';
 var SLEEP_TIME = 1000;
-var VERSION = "0.2.0";
-var LAST_UPDATE = "Jul 16, 2011";
+var VERSION = "0.2.0.1";
+var LAST_UPDATE = "Ago 02, 2011";
